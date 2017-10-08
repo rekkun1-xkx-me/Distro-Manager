1b9572f3f4579f4ab6489512c7a40e06de1635c7
IzeroCs <izero.cs@gmail.com>
2017-10-08 10:55:20 +0700

    Update

--- manager/gulpfile.js
+++ manager/gulpfile.js
@@ -8,12 +8,45 @@ var livereload  = require("gulp-livereload");
 var minifyCss   = require("gulp-cssnano");
 var minifyJs    = require("gulp-uglify");
 var cssbeautify = require("gulp-cssbeautify");
+var del         = require("del");
+
 
 gulp.task("compress_js", function() {
     gutil.log("Js is change");
 
-    return gulp.src("assets/javascript/dev/**/*.js")
-               .pipe(gulp.dest("assets/javascript"))
+    return gulp.src("assets/javascript/dev/*.js")
+               .pipe(rename({
+                    extname: ".unmin.js"
+               }))
+               .pipe(gulp.dest("assets/tmp"))
+               .pipe(plumber({
+                    errorHandler: function(error) {
+                        gutil.log(error.toString());
+                        this.emit("end");
+                    }
+               }))
+               .pipe(minifyJs())
+               .pipe(plumber({
+                    errorHandler: function(error) {
+                        gutil.log(error.toString());
+                        this.emit("end");
+                    }
+               }))
+               .pipe(rename({
+                    extname: ".minify.js"
+               }))
+               .pipe(gulp.dest("assets/tmp"))
+               .pipe(livereload());
+});
+
+gulp.task("compress_js_desktop", function() {
+    gutil.log("Js desktop is change");
+
+    return gulp.src("assets/javascript/dev/desktop/**/*.js")
+               .pipe(rename({
+                    extname: ".js"
+               }))
+               .pipe(gulp.dest("assets/javascript/desktop"))
                .pipe(plumber({
                     errorHandler: function(error) {
                         gutil.log(error.toString());
@@ -30,6 +63,25 @@ gulp.task("compress_js", function() {
                .pipe(rename({
                     extname: ".min.js"
                }))
+               .pipe(gulp.dest("assets/javascript/desktop"))
+               .pipe(livereload());
+});
+
+gulp.task("concat_js", function() {
+    gutil.log("Concat file js");
+
+    return gulp.src("assets/tmp/*.unmin.js")
+               .pipe(concat("app.js"))
+               .pipe(gulp.dest("assets/javascript"))
+               .pipe(livereload());
+});
+
+gulp.task("concat_js_min", function() {
+    gutil.log("Concat file js min");
+
+    return gulp.src("assets/tmp/*.unmin.minify.js")
+               .pipe(concat("app.min.js"))
+               .pipe(minifyJs())
                .pipe(gulp.dest("assets/javascript"))
                .pipe(livereload());
 });
@@ -100,7 +152,10 @@ gulp.task("watch", function() {
 
     gulp.watch([ "assets/theme/default/sass/*.scss", "assets/theme/default/sass/icomoon/*.scss" ], [ "sass" ]);
     gulp.watch([ "assets/theme/default/sass/desktop/*.scss" ], [ "sass_desktop" ]);
-    gulp.watch([ "assets/javascript/dev/**/*.js" ], [ "compress_js" ]);
+    gulp.watch([ "assets/javascript/dev/*.js" ], [ "compress_js" ]);
+    gulp.watch([ "assets/javascript/dev/desktop/**/*.js" ], [ "compress_js_desktop" ]);
+    gulp.watch([ "assets/tmp/*.unmin.js" ], [ "concat_js" ]);
+    gulp.watch([ "assets/tmp/*.unmin.minify.js" ], [ "concat_js_min" ]);
     gulp.watch([ "assets/theme/default/sass/icomoon/fonts/*.*"], [ "clone_icomoon_font" ]);
 });
 
@@ -108,6 +163,9 @@ gulp.task("default", [
     "sass",
     "sass_desktop",
     "compress_js",
+    "compress_js_desktop",
+    "concat_js",
+    "concat_js_min",
     "clone_icomoon_font",
     "watch"
 ]);
